1c0a4bd07581aa2720634a7ece1b8e2122137b66
ecem yelkanat <iremecemyelkanat@gmail.com>
2022-02-02 20:40:30 +0300

    created database schema and basic express app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+})
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/c', async (req, res) => {
+    const camp = new Campground({title: 'lele', description: 'lelelele'});
+    await camp.save();
+    res.send(camp)
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
